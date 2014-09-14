1467e67763cde3e3fb41090b53feb168d5d7ee66
sqweebking <sqweebking@gmail.com>
2014-09-14 04:30:03 -0400

    Adding scraper.py

---
+++ scrape.py
@@ -0,0 +1,49 @@
+# Reddit.com music subreddit link scraper - Mike Eder 2014
+#!/usr/bin/env python2
+
+import httplib2
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+
+http = httplib2.Http()
+
+# List possible subreddits
+subs = ['electronicmusic','dnb','liquiddnb','trance','house','trap']
+
+for item in enumerate(subs):
+	print str(item)
+try:
+	sub = int(raw_input('Select a subreddit to grab links from: '))
+except ValueError:
+	print "You must enter a valid number!"
+
+status, response = http.request('http://www.reddit.com/r/' + subs[sub])
+
+# Function to remove duplicate links
+def unique(items):
+	found = set([])
+	keep = []
+	
+	for item in items:
+		if item not in found:
+			found.add(item)
+			keep.append(item)
+	return keep
+
+# Arrays to hold links
+ytLinks = []
+scLinks = []
+
+# For loop to append found links
+for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+	if link.has_key('href') and 'https://www.youtube.com' in link['href']:
+		ytLinks.append(str(link['href']))
+	if link.has_key('href') and 'https://soundcloud.com' in link['href']:
+		scLinks.append(str(link['href']))
+
+# Output link lists
+print "Scraped links from www.reddit.com/r/" + subs[sub]
+print "YouTube:"
+print "\n".join(unique(ytLinks))
+print "SoundCloud:"
+print "\n".join(unique(scLinks))
+
