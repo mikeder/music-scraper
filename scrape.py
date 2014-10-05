cd3162683ad2efaf401ca7a3c62b62e0dbbcbe5c
sqweebking <sqweebking@gmail.com>
2014-10-05 02:19:44 -0400

    2am update

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 # Reddit.com music subreddit link scraper - Mike Eder 2014
 #! /usr/bin/python
 
-import httplib2, timeit, pafy, re, sys
+import httplib2, time, pafy, re, sys, os
 from bs4 import BeautifulSoup, SoupStrainer
 
 http = httplib2.Http()
@@ -40,19 +40,51 @@ for link in BeautifulSoup(response).find_all('a', href=True):
 ytLinks = unique(ytLinks)
 scLinks = unique(scLinks)
 
-# Function to perform youtube-dl on YT links
+# Array to hold source file paths
+sources = []
 
+# Function to perform youtube-dl on YT links
 def ytDL():
 	i = 0
+	tSize = []
+	start = time.time()
 	for link in ytLinks:
-		i = int(i)
-		video = pafy.new(ytLinks[i])
-		audio = video.getbestaudio()
-		title = re.sub('[/,.!@$#]', '', video.title)
-		print "Downloading: " + title + "." + audio.extension
-		audio.download(filepath=dir + title + "." + audio.extension)
-		i += 1
+		try:
+			video = pafy.new(ytLinks[i])
+			audio = video.getbestaudio()
+			title = re.sub('[/,.!@$#<>()]', '', video.title)
+			file = dir + title + '.' + audio.extension
+			size = audio.get_filesize() / 1048576
+			line = 'Downloading: %s - %sMB' % (file, str(size))
+			if os.path.isfile(file):
+				print '%s already exists, skipping' % file
+			else:
+				print line
+				audio.download(filepath=file)
+				sources.append(file)
+				print '%s' % ''*len(line)
+				tSize.append(size)
+			i += 1
+		except Exception:
+			print 'Error opening %s, skipping' % ytLinks[i]
+#			print sys.exc_info()[:2]
+			sys.exc_clear()
+			i += 1
+	tSize = sum(tSize)
+	end = time.time()
+	dlTime = round(end - start)
+	dlTimeStr = ''
+	if dlTime < 60:
+		dlTimeStr = ' seconds'
+	else:
+		dlTime = round(dlTime / 60)
+		dlTimeStr = ' minutes'
+	if sources:
+		print 'New files:\r'
+		print '\n'.join(sources)
+		print 'Total downloaded: %dMB in %d%s' % (tSize, dlTime, dlTimeStr)
+	else:
+		print 'No new files downloaded.'
 
-
+print 'Attempting to download %d new songs' % len(ytLinks)
 ytDL()
-#print str(" ".join(ytLinks))
