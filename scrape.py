2c847e47c81ee9c75311036c81aa2589c2bee2a8
sqweebking <sqweebking@gmail.com>
2014-10-09 07:55:41 -0400

    Pushing Dev to Master to fix earlier screwup

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
+
 #!/usr/bin/env python
 
 # RSDC - Reddit Scrape Download Convert - sqweebking 2014
+# scrape.py
+# DEV Version 1.1
 
 import httplib2
 import time
@@ -32,145 +35,135 @@ full = hyp + sub
 print 'Scraping links from: ' + full
 status, response = http.request(full)
 
-# Function to remove duplicate links
-def unique(items):
-	found = set([])
-	keep = []
-	
-	for item in items:
-		if item not in found:
-			found.add(item)
-			keep.append(item)
-	return keep
+def scrape():
+ # Arrays to hold links
+ ytLinks = []
+ scLinks = []
 
-# Arrays to hold links
-ytLinks = []
-scLinks = []
+ # For loop to append found links
+ for link in BeautifulSoup(response).find_all('a', href=True):
+  if 'www.youtube.com' in link['href']:
+   ytLinks.append(str(link['href']))
+  if 'soundcloud.com' in link['href']:
+   scLinks.append(str(link['href']))
+ # Remove Duplicates from link arrays
+ ytLinks = list(set(ytLinks))
+ scLinks = list(set(scLinks))
 
-# For loop to append found links
-for link in BeautifulSoup(response).find_all('a', href=True):
-	if 'www.youtube.com' in link['href']:
-		ytLinks.append(str(link['href']))
-	if 'soundcloud.com' in link['href']:
-		scLinks.append(str(link['href']))
-
-# Remove Duplicates from link arrays
-ytLinks = unique(ytLinks)
-scLinks = unique(scLinks)
-
-# Array to hold downloaded files path for conversion function
-sources = []
+ # Array to hold downloaded files path for conversion function
+ sources = []
+ return (ytLinks, scLinks)
 
 # Function to perform download sequence
-def ytDL():
-	i = 0
-	tSize = [] # array to hold file sizes for sum at the end
-	start = time.time() # start time for download timer
-	for link in ytLinks:
-		try:
-			link = ytLinks[i]
-			line1 = '%d. Opening: %s' % (i + 1, link)
-			print line1
-			video = pafy.new(link)
-			audio = video.getbestaudio() # selects best available audio 
-			title = re.sub('[\'/,;:.!@$#<>()]', '', str(video.title))
-			punks = '''!()[]{};:'"\,<>./?@#$%^&*_~'''
-			#title = str(video.title)
-			#title = fTitle.decode('utf-8').decode('ascii','ignore').encode('ascii')
-			file = inDir + title + '.' + audio.extension
-			size = audio.get_filesize() / 1048576	
-			line2 = '- Downloading to: %s - %sMB' % (file, str(size))
-			if os.path.isfile(file):
-				print '- Source file already exists, skipping'
-			else: # download audio if it doesn't already exist
-				print line2
-				audio.download(filepath=file)
-				sources.append(file)
-				print '%s' % ''*len(line2)
-				tSize.append(size)
-			i += 1
-		except Exception: # handle restricted/private videos etc.
-			err = sys.exc_info()[:2]
-	 	  print '** Problem: %s, skipping' % (err[1])
-			sys.exc_clear()
-			i += 1
-	tSize = sum(tSize)
-	end = time.time() # end time for download timer
-	dlTime = round(end - start)
-	dlTimeStr = ''
-	if dlTime < 60: # determine if seconds or minutes
-		dlTimeStr = ' seconds'
-	else:
-		dlTime = round(dlTime / 60)
-		dlTimeStr = ' minutes'
-	if sources: # if source array isn't empty display download info
-		print 'Total download: %d files, %dMB, in %d%s' % (len(sources), tSize, dlTime, dlTimeStr)
-	else: # source array is empty
-		print 'No new files downloaded.'
-
+def download():
+ i = 0
+ ytLinks, scLinks = scrape()
+ sources = []
+ tSize = [] # array to hold file sizes for sum at the end
+ start = time.time() # start time for download timer
+ print 'Attempting to download %d new songs' % len(ytLinks)
+ for link in ytLinks:
+  try:
+   link = ytLinks[i]
+   line1 = '%d. Opening: %s' % (i + 1, link)
+   print line1
+   video = pafy.new(link)
+   audio = video.getbestaudio() # selects best available audio 
+   title = re.sub('[\'/,;:.!@$#<>()]', '', str(video.title))
+   file = inDir + title + '.' + audio.extension
+   size = audio.get_filesize() / 1048576	
+   line2 = '- Downloading to: %s - %sMB' % (file, str(size))
+   if os.path.isfile(file):
+    print '- Source file already exists, skipping'
+   else: # download audio if it doesn't already exist
+    print line2
+    audio.download(filepath=file)
+    sources.append(file)
+    print '%s' % ''*len(line2)
+    tSize.append(size)
+   i += 1
+  except Exception: # handle restricted/private videos etc.
+   err = sys.exc_info()[:2]
+   print '** Problem: %s, skipping' % (err[1])
+   sys.exc_clear()
+   i += 1
+ tSize = sum(tSize)
+ end = time.time() # end time for download timer
+ dlTime = round(end - start)
+ dlTimeStr = ''
+ if dlTime < 60: # determine if seconds or minutes
+  dlTimeStr = ' seconds'
+ else:
+  dlTime = round(dlTime / 60)
+  dlTimeStr = ' minutes'
+ if sources: # if source array isn't empty display download info
+  print 'Total download: %d files, %dMB, in %d%s' % (len(sources), tSize, dlTime, dlTimeStr)
+ else: # source array is empty
+  print 'No new files downloaded.'
+ return sources
 # Function to process new files for artist, title, ext.
-def proc(i):
-	path = sources[i]
-	file = path[len(inDir):-4]
-	ext = path[-4:]
-	split = file.split('-')
-	where = file.find('-')
-	if len(split) > 2:
-		artist = split[0] + split[1]
-		title = split[2]
-	elif len(split) < 2:
-		artist = file
-		title = file
-	elif len(split) == 2:
-		artist = split[0]
-		title = split[1]
-	else:
-		split = file.split(' ')
-		artist = split[0]
-		title = split - split[0]
-	return (path, file, ext, artist, title)
+def proc(track):
+ path = track
+ file = path[len(inDir):-4]
+ ext = path[-4:]
+ split = file.split('-')
+ where = file.find('-')
+ if len(split) > 2:
+  artist = split[0] + split[1]
+  title = split[2]
+ elif len(split) < 2:
+  artist = file
+  title = file
+ elif len(split) == 2:
+  artist = split[0]
+  title = split[1]
+ else:
+  split = file.split(' ')
+  artist = split[0]
+  title = split - split[0]
+ return (path, file, ext, artist, title)
 	
 # Function to convert files to mp3
-def convert():
-	i = 0
-	c = 0
-	start = time.time()
-	try:
-		for file in sources:
-			(path, file, ext, artist, title) = proc(i)
-			inFile = AudioSegment.from_file(path)
-			comments = 'Ripped by music-scraper w/ help from pafy and pydub'
+def convert(sources):
+ i = 0
+ c = 0
+ start = time.time()
+ if not sources:
+  print 'No new files to convert :('
+ else:
+  try:
+   for file in sources:
+    track = sources[i]
+    (path, file, ext, artist, title) = proc(track)
+    inFile = AudioSegment.from_file(path)
+    comments = 'Ripped by music-scraper w/ help from pafy and pydub'
 # Export converted file in mp3 format to below dir
-			if artist == title:
-				outFile = '%s.mp3' % (artist)
-			else:
-				outFile = '%s-%s.mp3' % (artist, title)
-			
-			print '%d. Exporting: %s' % (i+1, outFile)
-			inFile.export(outDir + outFile, format='mp3', bitrate='192k', tags={'artist': artist, 'title': title, 'album': 'YT Rip', 'comments': comments})
-			c += 1		
-			i += 1
-	except:
-		err = sys.exc_info()[:2]
-                print '  **Problem** %s Export failed..' % (err[1])
-                sys.exc_clear()
-                i += 1
-	end = time.time()
-	cvTime = round(end - start)
-  cvTimeStr = ''
-  if cvTime < 60: # determine if in seconds or minutes
-    cvTimeStr = ' seconds'
-  else:
-    cvTime = round(cvTime / 60)
-    cvTimeStr = ' minutes'
-	if sources:
-		print 'Converted %d files in %d %s' % (c, cvTime, cvTimeStr)
-	else:
-		print 'No files to convert. :('
+    if artist == title:
+     outFile = '%s.mp3' % (artist)
+    else:
+     outFile = '%s-%s.mp3' % (artist, title)
+     print '%d. Exporting: %s' % (i+1, outFile)
+     inFile.export(outDir + outFile, format='mp3', bitrate='192k', tags={'artist': artist, 'title': title, 'album': 'YT Rip', 'comments': comments})
+     c += 1		
+     i += 1
+  except:
+   err = sys.exc_info()[:2]
+   print '  **Problem** %s Export failed..' % (err[1])
+   sys.exc_clear()
+   i += 1
+ end = time.time()
+ cvTime = round(end - start)
+ cvTimeStr = ''
+ if cvTime < 60: # determine if in seconds or minutes
+  cvTimeStr = ' seconds'
+ else:
+  cvTime = round(cvTime / 60)
+  cvTimeStr = ' minutes'
+ if sources:
+   print 'Converted %d files in %d %s' % (c, cvTime, cvTimeStr)
 
-print 'Attempting to download %d new songs' % len(ytLinks)
+# print 'Attempting to download %d new songs' % len(ytLinks)
 # Call the download function
-ytDL()
-print 'Converting files to MP3'
-convert()
-# indent fix
+scrape()
+sources = download()
+convert(sources)
